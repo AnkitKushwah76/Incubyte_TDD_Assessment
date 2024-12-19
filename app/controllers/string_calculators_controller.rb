00a58b4dafbe64cc1666091368b4691874a48d6f
Ankit Kushwah <ankitkushwahji159@gmail.com>
2024-12-19 14:36:31 +0530

    Implement feature: throws an exception for negative numbers

--- app/controllers/string_calculators_controller.rb
+++ app/controllers/string_calculators_controller.rb
@@ -1,11 +1,14 @@
 class StringCalculatorsController < ApplicationController
   def new
     @result = params[:result]
+    @error = params[:error]
   end
 
   def create
     result = StringCalculator.add(calculator_params[:numbers])
     redirect_to new_string_calculator_path(result: result)
+  rescue => e
+    redirect_to new_string_calculator_path(error: e.message)
   end
 
   private
