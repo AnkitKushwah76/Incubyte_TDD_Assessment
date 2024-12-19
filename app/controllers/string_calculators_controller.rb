a2015e25d13d41367dd62e703915c302e7f660f1
Ankit Kushwah <ankitkushwahji159@gmail.com>
2024-12-19 14:34:45 +0530

    Implement feature: String Calculator

--- app/controllers/string_calculators_controller.rb
+++ app/controllers/string_calculators_controller.rb
@@ -1,7 +1,16 @@
 class StringCalculatorsController < ApplicationController
   def new
+    @result = params[:result]
   end
 
   def create
+    result = StringCalculator.add(calculator_params[:numbers])
+    redirect_to new_string_calculator_path(result: result)
+  end
+
+  private
+
+  def calculator_params
+    params.require(:calculator).permit(:numbers)
   end
 end
--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -1,2 +1,22 @@
 class StringCalculator < ApplicationRecord
+  
+  def self.add(numbers)
+    return 0 if numbers.empty?
+
+    delimiter = ','
+    num_list = numbers.split(delimiter)
+
+    result_array = num_list.flat_map do |element|
+      element.split('"').flat_map do |part|
+        part.split(/\\n|;/)
+      end.map(&:to_i)
+    end
+    handle_negatives(result_array)
+    result_array.sum
+  end
+
+  def self.handle_negatives(numbers_array)
+    negatives = numbers_array.select { |num| num.negative? }
+    raise "Negative numbers not allowed: #{negatives.join(',')}" if negatives.any?
+  end
 end
